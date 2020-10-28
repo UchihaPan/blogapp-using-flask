3b70626270af4150fdebcb70b1fde7c3180337e5
panka <pankajshinde798@gmail.com>
2020-10-28 17:13:25 +0530

    third

--- blog_app/__init__.py
+++ blog_app/__init__.py
@@ -1,2 +1,6 @@
 from flask import Flask
+
 app = Flask(__name__)
+from blog_app.main.views import main
+
+app.register_blueprint(main)
---
+++ blog_app/main/views.py
@@ -0,0 +1,13 @@
+from flask import render_template, Blueprint, request
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/')
+def home():
+    return render_template('main/home.html')
+
+
+@main.route('/about')
+def about():
+    return render_template('main/about.html')
