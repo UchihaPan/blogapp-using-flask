5b77c7d2b7806010f0dbe8af6704ac1e2ae9bfa1
panka <pankajshinde798@gmail.com>
2020-11-02 18:04:36 +0530

    h

--- blog_app/__init__.py
+++ blog_app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask import Flask
 from blog_app.main.views import main
 from blog_app.error_handling.handers import error_handler
+from flask_login import LoginManager
 
 app = Flask(__name__)
 basedirectory = os.path.abspath(os.path.dirname(__file__))
@@ -11,6 +12,10 @@ app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedirector
 app.config['SQLAlchemy_TRACK_MODIFICATIONS'] = False
 database = SQLAlchemy()
 Migrate(app, database)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
 
 app.register_blueprint(main)
 app.register_blueprint(error_handler)
--- blog_app/models.py
+++ blog_app/models.py
@@ -1,22 +1,28 @@
 from blog_app import database
-from werkzeug import generate_password_hash,check_password_hash
-from flask_login import UserMixin
+from werkzeug import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_manager
+from datetime import datetime
 
-class user:
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+class User(database.Model, UserMixin):
     __tablename__ = 'users'
     id = database.column(database.Integer, primary_key=True)
     profile = database.column(database.String(20), nullable=False, defalult='a.jpg')
-    username=database.column(database.String(64),unique=True,index=True)
+    username = database.column(database.String(64), unique=True, index=True)
 
-    email=database.column(database.String(64),unique=True,index=True)
-    password_hash=database.column(database.String(64))
+    email = database.column(database.String(64), unique=True, index=True)
+    password_hash = database.column(database.String(64))
 
-    posts=database.relationship('posts',backref='bhaibhai',lazy=True)
+    posts = database.relationship('posts', backref='bhaibhai', lazy=True)
 
-    def __init__(self,username,email,password):
-        self.email=email
-        self.username=username
-        self.password_hash=generate_password_hash(password)
-    def check_password(self,password):
-        return check_password_hash(self.password_hash,password)
+    def __init__(self, username, email, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
